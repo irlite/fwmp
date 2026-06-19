ca5952ea627e520b6cc4daf729ae435dbb40e248
pathakutkarsh <utkarshp2001@gmail.com>
2026-06-19 16:45:51 +0200

    scorep changes

--- setup.sh
+++ setup.sh
@@ -5,13 +5,15 @@ module purge
 module load gcc
 module load openmpi
 module load python
+module load scorep
 
-gcc -O3 -fopenmp -shared -fPIC src/elastic_kernels.c -o src/libelastic_kernels.so
+scorep gcc -O3 -fopenmp -shared -fPIC src/elastic_kernels.c -o src/libelastic_kernels.so
 
-gcc -O3 -shared -fPIC src/elastic_kernels_single_threaded.c -o src/libelastic_kernels_single_threaded.so
+#gcc -O3 -shared -fPIC src/elastic_kernels_single_threaded.c -o src/libelastic_kernels_single_threaded.so
+scorep gcc -O3 -shared -fPIC elastic_kernels_single_threaded.c -o libelastic_kernels_single_threaded.so
 
 source .venv/bin/activate
 
 python -m pip install --upgrade pip
 
-python -m pip install numpy numba mpi4py h5py segyio matplotlib
+python -m pip install scorep numpy numba mpi4py h5py segyio matplotlib
